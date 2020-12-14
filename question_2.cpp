bce980e2097af9bc63f7810d1116317a6b5d975f
Fellow-fiend <72641329+Fellow-fiend@users.noreply.github.com>
2020-12-14 11:58:14 +0300

    Update question_2.cpp

--- question_2.cpp
+++ question_2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <cmath>
+
 int main(int argc, char *argv[])
 {
 	using namespace std;
 	double a, b, c;
 	cout << "Enter the a, b, c:\n";
-	cin >> a >> b >> c;
-	a == 0 ? throw("Error! a = 0.") : b*b - 4*a*c >= 0 ? 
-	cout << "x1 = " << -b/(2*a) + sqrt(b*b - 4*a*c)/(2*a) 
-	<< "\nx2 = " << -b/(2*a) - sqrt(b*b - 4*a*c)/(2*a) 
-	<< endl : cout << "x is not real\n";
+	if(!(cin >> a >> b >> c)) throw;
+	if(a == 0)
+	    cout << "Error! a = 0. Programm is terminated.";
+	else if(b*b - 4*a*c >= 0)
+	    cout << "x1 = " << -b/(2*a) + sqrt(b*b - 4*a*c)/(2*a) 
+	<< "\nx2 = " << -b/(2*a) - sqrt(b*b - 4*a*c)/(2*a) << endl;
+	    else cout << "x is not real\n";
+	
 	return 0;
 }
